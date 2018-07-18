c0c6a4f15122a78ab5b4e3e88485a824f0eb500a
kazoo <kazuki_saito@diveintocode.jp>
2018-07-18 16:18:00 +0900

    feat: 2.8 inspecting click and Drawing circle

---
+++ example/ch02/ex02-08-1/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  'use strict';
+  paper.install(window);
+  paper.setup(document.getElementById('mainCanvas'));
+
+  let tool = new Tool();
+
+  tool.onMouseDown = function(event) {
+    let c = Shape.Circle(event.point.x, event.point.y, 20);
+    c.fillColor = 'lightgreen';
+  };
+});
