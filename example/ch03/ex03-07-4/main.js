8fdb1515ebaf144faf99f2c6486f538b2d271319
kazoo <kazuki_saito@diveintocode.jp>
2018-07-20 09:56:40 +0900

    feat: ex-03-07-4 to ex-03-07-8 added

---
+++ example/ch03/ex03-07-4/main.js
@@ -0,0 +1,33 @@
+const multiline = "line1\
+line2";
+console.log(multiline);
+
+// ex03-07-5
+
+const multiline1 = "line1\n\
+line2";
+console.log(multiline1);
+
+// ex03-07-6
+
+const multiline2 = `line1
+line2
+line3
+line4`;
+console.log(multiline2);
+
+// ex03-07-7
+
+const multiline3 = `line1
+line2
+line3
+line4
+line5`;
+console.log(multiline3);
+
+// ex03-07-8
+
+const multiline4 = "line1\n" +
+  "line2\n" +
+  "line2\n";
+console.log(multiline4);
